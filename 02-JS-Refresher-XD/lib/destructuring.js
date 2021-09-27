b5a23f7a008910ef0aa616c4a4e489a7fe597799
Jin Mac jinctmac@gmail.com <jinctmac@gmail.com>
2021-09-27 17:55:26 +0100

    destructuring in js pulling individual elements from an array

---
+++ 02-JS-Refresher-XD/lib/destructuring.js
@@ -0,0 +1,6 @@
+// accessing individual array elements
+// and then assigning them to variables based on index position
+const letters = ["a","b","c"];
+[lettera, letterb] = letters;
+
+console.log(lettera, letterb);

40608395162c4760723d49d0a3244a30d1c07e4b
Jin Mac jinctmac@gmail.com <jinctmac@gmail.com>
2021-09-30 17:25:56 +0100

    adding vanilla JS logic to components

--- 03-React-Basics/01-starting-setup/src/components/expense_item.jsx
+++ 03-React-Basics/01-starting-setup/src/components/expense_item.jsx
@@ -4,11 +4,20 @@ const ExpenseItem = (props) => {
   // const expenseDate = new Date(2021, 2, 28);
   // const expenseTitle = 'Car Insurance';
   // const expenseAmount = 294.67;
+  const expenseMonth = props.date.toLocaleString('en-US', { month: 'long' });
+  const expenseDay = props.date.toLocaleString('en-US', { day: '2-digit'});
+  const expenseYear = props.date.getFullYear();
+
 
   return (
     <div className="expense-item">
       {/* convert to iso string for readability */}
-      <div>{props.date.toISOString()}</div>
+      <div>
+        {/* {props.date.toISOString()} */}
+        <div>{expenseMonth}</div>
+        <div>{expenseYear}</div>
+        <div>{expenseDay}</div>
+      </div>
       <div className="expense-item__description">
         <h2>{props.title}</h2>
         <div className="expense-item__price">£{props.amount}</div>

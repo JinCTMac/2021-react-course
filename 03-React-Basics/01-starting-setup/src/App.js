401163dc030e7af1440abef1403310a369b1da0c
Jin Mac jinctmac@gmail.com <jinctmac@gmail.com>
2021-10-03 17:45:40 +0100

    commit to check if git push is working properly

--- 03-React-Basics/01-starting-setup/src/App.js
+++ 03-React-Basics/01-starting-setup/src/App.js
@@ -27,6 +27,7 @@ function App() {
     <div>
       <h2>Let's get started!</h2>
       <p>This is also visible!</p>
+      <p>Cool</p>
       <ExpenseItem title={expenses[0].title} amount={expenses[0].amount} date={expenses[0].date}></ExpenseItem>
       <ExpenseItem title={expenses[1].title} amount={expenses[1].amount} date={expenses[1].date}></ExpenseItem>
       <ExpenseItem title={expenses[2].title} amount={expenses[2].amount} date={expenses[2].date}></ExpenseItem>

18063c607ca8d7523e77d103788d60e5fa96a573
Jin Mac jinctmac@gmail.com <jinctmac@gmail.com>
2021-10-04 15:17:24 +0100

    added date as a attribute to it can be passed along via props

--- 03-React-Basics/01-starting-setup/src/components/expense_item.jsx
+++ 03-React-Basics/01-starting-setup/src/components/expense_item.jsx
@@ -12,7 +12,7 @@ const ExpenseItem = (props) => {
     <div className="expense-item">
       {/* in here we can call our expenseDate component
       so we can call components inside of components */}
-      <ExpenseDate></ExpenseDate>
+      <ExpenseDate date={props.date}></ExpenseDate>
       <div className="expense-item__description">
         <h2>{props.title}</h2>
         <div className="expense-item__price">£{props.amount}</div>

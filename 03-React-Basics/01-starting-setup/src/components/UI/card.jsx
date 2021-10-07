39d1b22950056c3046e500df84714613781361db
Jin Mac jinctmac@gmail.com <jinctmac@gmail.com>
2021-10-07 17:54:21 +0100

    added comments to better explain props.children and the need to add classes to be affected by multiple css files

--- 03-React-Basics/01-starting-setup/src/components/UI/card.jsx
+++ 03-React-Basics/01-starting-setup/src/components/UI/card.jsx
@@ -1,9 +1,12 @@
 import "./card.css";
 
 const Card = (props) => {
+  // in order for the card component to be affected by other css classes given to the elements that call it (expenses), we need to make it take the props.className, aka the classnames of its parent element
   const classes = 'card ' + props.className;
+  // we can then add that as the className for what the card component returns, so it is affected by the card.css as well as the expenses.css
   return (
     <div className={classes}>{props.children}</div>
+    // props.children is a universal attribute available to all components, that essentially returns the HTML that is contained within the parent element that calls the card component
   )
 }
 

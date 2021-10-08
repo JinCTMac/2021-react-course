2a2fa7aa028273709987bd94910bc19341c1c527
Jin Mac jinctmac@gmail.com <jinctmac@gmail.com>
2021-10-08 17:56:26 +0100

    more comments to improve understanding

--- 03-React-Basics/01-starting-setup/src/components/Expenses/expenses.jsx
+++ 03-React-Basics/01-starting-setup/src/components/Expenses/expenses.jsx
@@ -6,6 +6,7 @@ const Expenses = (props) => {
   return (
     <Card className="expenses">
       {/* we pass down the expenses array as the expenses attribute, so we can call props.expenses[0].title for example */}
+      {/* in the card.jsx, we append the prop.className so that we get access to the css applied for the className expenses */}
       <ExpenseItem title={props.expenses[0].title} amount={props.expenses[0].amount} date={props.expenses[0].date}></ExpenseItem>
       <ExpenseItem title={props.expenses[1].title} amount={props.expenses[1].amount} date={props.expenses[1].date}></ExpenseItem>
       <ExpenseItem title={props.expenses[2].title} amount={props.expenses[2].amount} date={props.expenses[2].date}></ExpenseItem>

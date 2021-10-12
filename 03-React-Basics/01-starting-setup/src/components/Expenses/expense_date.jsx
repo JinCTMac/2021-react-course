f34bd8337bff7183199d3594242b240602a28583
Jin Mac jinctmac@gmail.com <jinctmac@gmail.com>
2021-10-12 15:16:26 +0100

    more defined explanation for date variables

--- 03-React-Basics/01-starting-setup/src/components/Expenses/expense_date.jsx
+++ 03-React-Basics/01-starting-setup/src/components/Expenses/expense_date.jsx
@@ -2,6 +2,8 @@ import "./expense_date.css";
 
 const ExpenseDate = (props) => {
   // when splitting date into its own Component, we need to get props again to access the data from the App.js
+  // it is better to make static variables to contain the logic
+  // rather than implementing the logic within the jsx code itself
   const expenseMonth = props.date.toLocaleString('en-US', { month: 'long' });
   const expenseDay = props.date.toLocaleString('en-US', { day: '2-digit'});
   const expenseYear = props.date.getFullYear();
